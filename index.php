47d22fe5e9a112e8feda360bdb7e4937324f66bb
ecbangga <ecbangga@gmail.com>
2020-11-04 22:04:11 +0800

    index of class user

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <?php
+            class User
+            {
+              public $name;
+              public $username;
+              public $email;
 
-echo "Hello World";
-?>
+              function addFriend($name){
+                echo "$name"." ."."Added a friend <br>";
+              }
+              function postStatus($name){
+                echo "$name"." ."."Posted a status <br>";
+              }
+            }
+            $example = new User();
+            echo "<br>";
+            echo $example->addFriend('Elvin Christian');
+            echo "<br>";
+            echo $example->postStatus('Elvin Christian');
+
+      ?>
+  </body>
+</html>
